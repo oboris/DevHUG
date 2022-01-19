58c06b4c7ed06bc57d226504aa3d9d710c3bb6ef
Onyshchenko Borys <onischenkoboris@gmail.com>
2022-01-20 01:48:41 +0200

    some actions but fail

---
+++ app/src/main/java/info/goodlift/devhug/Devices.kt
@@ -0,0 +1,39 @@
+package info.goodlift.devhug
+
+import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import com.google.android.things.pio.Gpio
+
+class Devices {
+    private val TAG = "EasyRainbowHat"
+
+    private var initButtonA = false
+    private var initButtonB = false
+    private var initButtonC = false
+    private var initLedRed = false
+    private var initLedGreen = false
+    private var initLedBlue = false
+
+    val buttonA: Button by lazy { initButtonA = true; RainbowHat.openButtonA() }
+    val buttonB: Button by lazy { initButtonB = true; RainbowHat.openButtonB() }
+    val buttonC: Button by lazy { initButtonC = true; RainbowHat.openButtonC() }
+    val ledRed: Gpio by lazy { initLedRed = true; RainbowHat.openLedRed() }
+    val ledGreen: Gpio by lazy { initLedGreen = true; RainbowHat.openLedGreen() }
+    val ledBlue: Gpio by lazy { initLedBlue = true; RainbowHat.openLedBlue() }
+
+    fun Button.onClick(onPress: (Boolean) -> Unit) = setOnButtonEventListener { _, pressed -> onPress(pressed) }
+
+    fun Gpio.on() { value = true }
+
+    fun Gpio.off() { value = false }
+
+    fun onDestroy() {
+        if (initButtonA) buttonA.close()
+        if (initButtonB) buttonB.close()
+        if (initButtonC) buttonC.close()
+
+        if (initLedRed) ledRed.close()
+        if (initLedGreen) ledGreen.close()
+        if (initLedBlue) ledBlue.close()
+    }
+}
--- app/src/main/java/info/goodlift/devhug/MainActivity.kt
+++ app/src/main/java/info/goodlift/devhug/MainActivity.kt
@@ -9,6 +9,9 @@ import com.google.android.things.pio.PeripheralManager
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    val hat = Devices()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +21,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val manager=PeripheralManager.getInstance()
         Log.d(TAG, "Available GPIO: " + manager.gpioList)
+
+        hat.apply {
+            buttonA.onClick {
+                if (it) {
+                    if(ledRed.value){
+                        ledRed.off()
+                    }
+                    else{
+                        ledRed.on()
+                    }
+
+                }
+                buttonB.onClick {
+
+                }
+                buttonC.onClick {
+                    if (it) {
+                        ledRed.on()
+                        ledGreen.off()
+                        ledBlue.on()
+
+                    }
+                }
+            }
+        }
     }
 
     companion object{
@@ -26,5 +54,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(p0: View?) {
         exitProcess(-1)
+    }
+
+    override fun onDestroy() {
+        hat.onDestroy()
+        super.onDestroy()
     }
 }
